4177ae715bce3e51e5d5cb2d84244c54eae4cffc
Alex Pavlovic <lex.pavlovic@gmail.com>
2018-02-23 20:32:39 +0000

    setting up recipe-list-component for recipes

--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+      new Recipe('A Test Recipe', 'This is simply a test', 'https://images.unsplash.com/photo-1504387828636-abeb50778c0c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f6251a0ab3853414a3169bfd5e79f164&auto=format&fit=crop&w=634&q=80')
+    ];
 
 
   constructor() { }
