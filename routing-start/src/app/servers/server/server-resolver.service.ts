e14b9fe1c969a7bb51888cd67c4f1956c269afe0
Alex Pavlovic <lex.pavlovic@gmail.com>
2018-04-07 12:37:48 +0000

    fix impots and typos

--- routing-start/src/app/servers/server/server-resolver.service.ts
+++ routing-start/src/app/servers/server/server-resolver.service.ts
@@ -1,6 +1,6 @@
-import { Resolve ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } form 'rxjs/Observable';
-import { Injectable } from '@angular/core";
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
 
 import { ServersService } from '../servers.service';
 
@@ -14,7 +14,7 @@ interface Server {
 export class ServerResolver implements Resolve<Server> {
     constructor(private serversService: ServersService) {}
     
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Ovservable<Server> |
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Server> |
     Promise<Server> | Server {
         return this.serversService.getServer(route.params['id']);
     }
