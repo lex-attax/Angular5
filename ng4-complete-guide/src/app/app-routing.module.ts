67611693af9c8d9704647c0207a0abff5d041dbe
Alex Pavlovic <lex.pavlovic@gmail.com>
2018-04-11 05:36:03 +0000

    fix imports

--- ng4-complete-guide/src/app/app-routing.module.ts
+++ ng4-complete-guide/src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
+import { Routes, RouterModule } from '@angular/router';
 
-const appRoutes: Routes [
-    { path: 'recipes', component: RecipesComponent }
-    ]
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/recipes' },    
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'shopping-list', component: ShoppingListComponent }
+];
 
 
 // Transform from a normal TypeScript class to a Module
 @NgModule({
-    
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
     
