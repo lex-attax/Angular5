bf19ddf542cb8bc60e44d0383a31087dcc053d33
Alex Pavlovic <lex.pavlovic@gmail.com>
2018-04-11 05:19:41 +0000

    add recipes route

--- ng4-complete-guide/src/app/app-routing.module.ts
+++ ng4-complete-guide/src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
 
-
+const appRoutes: Routes [
+    { path: 'recipes', component: RecipesComponent }
+    ]
 
 
 // Transform from a normal TypeScript class to a Module
