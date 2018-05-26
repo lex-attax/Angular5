d35fe4bc102517886b4f25e915bd0f31d797929e
Alex Pavlovic <lex.pavlovic@gmail.com>
2018-05-26 09:28:39 +0000

    add observable

--- observables-start/src/app/app-routing.module.ts
+++ observables-start/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'user/:id', component: UserComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
-  declarations: []
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- observables-start/src/app/app.module.ts
+++ observables-start/src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
 import { HomeComponent } from './home/home.component';
-
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    HomeComponent
+    HomeComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
